e3426b29567a7dfb45e2b7c09168a214ff16e212
William P. Pinto <william.pinto@lambda3.com.br>
2020-02-01 09:40:54 -0300

    Class 01: added babel-loader to project and using it to pre-compile the files before webpack can use then; it throws an error that System.register is not supported, because we still using the Systemjs in the babelrc file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
     output: {
         filename: 'bundle-file.js',
         path: pathToDistfolder
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,  //Regular expression to inform that webpack shall get all the files that ends with .js
+                exclude: /node-modules/, //Ignores all files inside the node-modules
+                use: {
+                    loader: 'babel-loader' //Informs how webpack shall process the file. We use babel-loader so the typescript scripts inside our project could be processed as a JS code before webpack process it and creathe the bundle file
+                }
+            }
+        ]
     }
 }
